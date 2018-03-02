c0b84331dd70af70fb6f2206fbb38141702989b1
Alina Okolotovich <alina.okolotovich@targetprocess.com>
2018-03-02 16:28:37 +0300

    adding scr files to repo

---
+++ src/main/java/Triangle/src/runner/Main.java
@@ -0,0 +1,16 @@
+package Triangle.src.runner;
+import Triangle.src.triangle.Triangle;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+
+		Triangle tr = new Triangle(1.0, 2.0, 2.0);
+		System.out.println(tr.checkTriangle());
+		System.out.println(tr.getMessage());
+		System.out.println(tr.detectTriangle());
+		
+	}
+
+}
