ed4a6a6d2e20b595f880c74a5eb4529907cf3ba5
Alina Okolotovich <alina.okolotovich@targetprocess.com>
2018-03-13 18:48:20 +0300

    refactoring of GetSquireTest adding data provider

--- src/test/java/GetSquareTest.java
+++ src/test/java/GetSquareTest.java
@@ -1,4 +1,5 @@
 import Triangle.triangle.Triangle;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
@@ -7,128 +8,51 @@ import static org.testng.Assert.assertEquals;
  */
 public class GetSquareTest {
     Utils ut = new Utils();
-    @Test
-    public void correctSquareCalculation() {
-        double a = 5;
-        double b = 3;
-        double c = 7;
-        Triangle tr = new Triangle(a, b, c);
-        assertEquals(6.49, tr.getSquare(), 0.01);
+    @DataProvider(name = "Correct Triangle")
+    public static Object [][] correctTriangleSides(){
+        Utils ut = new Utils();
+        return new Object[][]{
+                {5d, 3d, 7d, "correct triangle"},
+        };
+    }
+    @DataProvider (name = "Incorrect Triangle")
+    public static Object [][] oneSidesIsZero() {
+        Utils ut = new Utils();
+        return new Object[][]{
+                {0d, ut.generateRandomDouble(0, 100), ut.generateRandomDouble(0, 100), "a = 0"},
+                {ut.generateRandomDouble(0, 100), 0d, ut.generateRandomDouble(0, 100), "b = 0"},
+                {ut.generateRandomDouble(0, 100), ut.generateRandomDouble(0, 100), 0d, "c = 0"},
+                {0d, 0d, ut.generateRandomDouble(0, 100), "a = 0, b = 0"},
+                {ut.generateRandomDouble(0, 100), 0d, 0d, "b = 0, c = 0"},
+                {0d, ut.generateRandomDouble(0, 100), 0d, "a = 0, c = 0"},
+                {0d, 0d, 0d, "a = 0, b = 0, c = 0"},
+                {ut.generateRandomDouble(-100, 0), ut.generateRandomDouble(0, 100), ut.generateRandomDouble(0, 100), "a < 0"},
+                {ut.generateRandomDouble(100, 0), ut.generateRandomDouble(-100, 0), ut.generateRandomDouble(0, 100), "b < 0"},
+                {ut.generateRandomDouble(100, 0), ut.generateRandomDouble(0, 100), ut.generateRandomDouble(-100, 0), "c < 0"},
+                {ut.generateRandomDouble(-100, 0), ut.generateRandomDouble(-100, 0), ut.generateRandomDouble(0, 100), "a < 0, b < 0"},
+                {ut.generateRandomDouble(-100, 0), ut.generateRandomDouble(0, 100), ut.generateRandomDouble(-100, 0), "a < 0, c < 0"},
+                {ut.generateRandomDouble(0, 100), ut.generateRandomDouble(-100, 0), ut.generateRandomDouble(-100, 0), "b < 0, c < 0"},
+                {ut.generateRandomDouble(-100, 0), ut.generateRandomDouble(-100, 0), ut.generateRandomDouble(-100, 0), "a < 0, b < 0, c < 0"},
+                {3.27d, 2.73d, 6d, "a + b = c"},
+                {28.001d, 100d, 71.999d, "a + c = b"},
+                {2.17d, 2.079d, 0.091d, "b + c = a"},
+                {3d, 7d, 11d, "a + b < c"},
+                {3d, 11d, 7d, "a + c < b"},
+                {11d, 7d, 3d, "b + c < a"}
+        };
     }
 
-    @Test(expectedExceptions = Exception.class)
-    public void SquareExceptionIfSideIsZero(){
+    @Test(dataProvider = "Correct Triangle")
+    public void correctSquareCalculation(Double a, Double b, Double c, String message) {
+        Triangle tr = new Triangle(a, b, c);
+        assertEquals(6.49, tr.getSquare(), 0.01);
+        System.out.println(message);
+    }
+
+    @Test(dataProvider = "Incorrect Triangle", expectedExceptions = Exception.class)
+    public void squareExceptionIfSidesNotFormTriangle(double a, double b, double c, String message){
         //checks that application throws an exception if one of the side is zero
-        double a = 0;
-        double b = ut.generateRandomDouble(0,100);
-        double c = ut.generateRandomDouble(0,100);
-        double a1 = ut.generateRandomDouble(0,100);
-        double b1 = 0;
-        double c1 = ut.generateRandomDouble(0,100);
-        double a2 = ut.generateRandomDouble(0,100);
-        double b2 = ut.generateRandomDouble(0,100);
-        double c2 = 0;
         Triangle tr = new Triangle(a, b, c);
-        tr.getSquare();
-        Triangle tr1 = new Triangle(a1, b1, c1);
-        tr1.getSquare();
-        Triangle tr2 = new Triangle(a2, b2, c2);
-        tr2.getSquare();
-    }
-    @Test(expectedExceptions = Exception.class)
-    public void SquareExceptionIfTwoSidesAreZero(){
-        //checks that application throws an exception if 2 sides are zero
-        double a = 0;
-        double b = 0;
-        double c = ut.generateRandomDouble(0,100);
-        double a1 = ut.generateRandomDouble(0,100);
-        double b1 = 0;
-        double c1 = 0;
-        double a2 = 0;
-        double b2 = ut.generateRandomDouble(0,100);
-        double c2 = 0;
-        Triangle tr = new Triangle(a, b, c);
-        tr.getSquare();
-        Triangle tr1 = new Triangle(a1, b1, c1);
-        tr1.getSquare();
-        Triangle tr2 = new Triangle(a2, b2, c2);
-        tr2.getSquare();
-    }
-    @Test(expectedExceptions = Exception.class)
-    public void SquareExceptionAllSidesAreZero(){
-        //checks that application throws an exception if 3 sides are zero
-        double a = 0;
-        double b = 0;
-        double c = 0;
-        Triangle tr = new Triangle(a, b, c);
-        tr.getSquare();
-    }
-    @Test(expectedExceptions = Exception.class)
-    public void SquareExceptionIfSideIsNegative(){
-        //checks that application throws an exception if one of the side is negative
-        double a = ut.generateRandomDouble(-100, 0);
-        double b = ut.generateRandomDouble(0,100);
-        double c = ut.generateRandomDouble(0,100);
-        double a1 = ut.generateRandomDouble(0,100);
-        double b1 = ut.generateRandomDouble(-100, 0);
-        double c1 = ut.generateRandomDouble(0,100);
-        double a2 = ut.generateRandomDouble(0,100);
-        double b2 = ut.generateRandomDouble(0,100);
-        double c2 = ut.generateRandomDouble(-100,0);
-        Triangle tr = new Triangle(a, b, c);
-        tr.getSquare();
-        Triangle tr1 = new Triangle(a1, b1, c1);
-        tr1.getSquare();
-        Triangle tr2 = new Triangle(a2, b2, c2);
-        tr2.getSquare();
-    }
-    @Test(expectedExceptions = Exception.class)
-    public void SquareExceptionIfTwoSidesAreNegative(){
-        //checks that application throws an exception if two sides are negative
-        double a = ut.generateRandomDouble(-100,0);
-        double b = ut.generateRandomDouble(-100,0);
-        double c = 7;
-        double a1 = 5;
-        double b1 = ut.generateRandomDouble(-100,0);
-        double c1 = ut.generateRandomDouble(-100,0);
-        double a2 = ut.generateRandomDouble(-100,0);
-        double b2 = 3;
-        double c2 = ut.generateRandomDouble(-100,0);
-        Triangle tr = new Triangle(a, b, c);
-        tr.getSquare();
-        Triangle tr1 = new Triangle(a1, b1, c1);
-        tr1.getSquare();
-        Triangle tr2 = new Triangle(a2, b2, c2);
-        tr2.getSquare();
-    }
-    @Test(expectedExceptions = Exception.class)
-    public void SquareExceptionIfAllSidesAreNegative(){
-        //checks that application throws an exception if 3 sides are negative
-        double a = ut.generateRandomDouble(-100,0);
-        double b = ut.generateRandomDouble(-100,0);
-        double c = ut.generateRandomDouble(-100,0);
-        Triangle tr = new Triangle(a, b, c);
-        tr.getSquare();
-    }
-    @Test(expectedExceptions = Exception.class)
-    public void SquareExceptionIfSidesNotFormTriangle(){
-        //checks that application throws an exception if a + b = c
-        double a = ut.generateRandomDouble(0,100);
-        double b = ut.generateRandomDouble(0,100);
-        double c = a + b;
-        Triangle tr = new Triangle(a, b, c);
-        tr.getSquare();
-        //checks that application throws an exception if a + c = b
-        double a1 = ut.generateRandomDouble(0,100);
-        double c1 = ut.generateRandomDouble(0,100);
-        double b1 = a1 + c1;
-        Triangle tr1 = new Triangle(a1, b1, c1);
-        tr.getSquare();
-        //checks that application throws an exception if c + b = a
-        double c2 = ut.generateRandomDouble(0,100);
-        double b2 = ut.generateRandomDouble(0,100);
-        double a2 = c2 + b2;
-        Triangle tr2 = new Triangle(a2, b2, c2);
         tr.getSquare();
     }
 }
